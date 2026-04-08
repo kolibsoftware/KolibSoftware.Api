525dde3615ecd448ca018ad99b789a30c2ac95a0
janitochvvll2311 <janitochvvll2311@gmail.com>
2026-04-08 13:01:36 -0600

    Enhance XML documentation for IQuery and IPageQuery interfaces

--- KolibSoftware.Api.Infra/Repo/IPageQuery.cs
+++ KolibSoftware.Api.Infra/Repo/IPageQuery.cs
@@ -1,7 +1,19 @@
 namespace KolibSoftware.Api.Infra.Repo;
 
+/// <summary>
+/// Represents a paginated query that can be applied to an IQueryable. This interface extends IQuery with pagination parameters (page number and page size). The repository will use these parameters to return a paginated result when the query is executed.
+/// </summary>
+/// <typeparam name="T"></typeparam>
 public interface IPageQuery<T> : IQuery<T>
 {
+
+    /// <summary>
+    /// The page number to return. This should be a positive integer (1-based index). The repository will use this parameter to determine which page of results to return when the query is executed.
+    /// </summary>
     public int PageNumber { get; }
+
+    /// <summary>
+    /// The page size to return. This should be a positive integer. The repository will use this parameter to determine how many items to return in the page when the query is executed.
+    /// </summary>
     public int PageSize { get; }
 }
--- KolibSoftware.Api.Infra/Repo/IQuery.cs
+++ KolibSoftware.Api.Infra/Repo/IQuery.cs
@@ -1,6 +1,17 @@
 namespace KolibSoftware.Api.Infra.Repo;
 
+/// <summary>
+/// Represents a query that can be applied to an IQueryable.
+/// </summary>
+/// <typeparam name="T"></typeparam>
 public interface IQuery<T>
 {
+
+    /// <summary>
+    /// Applies the query to the given IQueryable. This method should not execute the query, but rather return a new IQueryable that represents the query. The repository will then execute the query when it is ready to get the results.
+    /// </summary>
+    /// <param name="queryable"></param>
+    /// <returns></returns>
     IQueryable<T> Apply(IQueryable<T> queryable);
+
 }
