8f74043cd7883cecd6b17ff6982e1479cd801318
janitochvvll2311 <janitochvvll2311@gmail.com>
2026-04-14 12:56:09 -0600

    feat: Update DocumentTask progress tracking and stages

--- KolibSoftware.Api.Example/Documents/DocumentTask.cs
+++ KolibSoftware.Api.Example/Documents/DocumentTask.cs
@@ -6,6 +6,7 @@ namespace KolibSoftware.Api.Example.Documents;
 [Task]
 public class DocumentTask
 {
+    public string Progress { get; set; } = "0%";
     public string Path { get; set; } = string.Empty;
     public string Stage { get; set; } = "Init";
 }
@@ -21,19 +22,24 @@ public class DocumentTaskHandler(
         {
             case "Init":
                 task.Stage = "Extract";
+                task.Progress = "0%";
                 var extractTask = new ExtractTask { Path = task.Path };
                 return TaskResult.Waiting(task, [extractTask]);
             case "Extract":
                 var extractResult = dependencies.OfType<ExtractTask>().FirstOrDefault() ?? throw new Exception("Extract task result not found");
                 task.Stage = "Summarize";
                 var summaryTask = new SummaryTask { Documents = extractResult.DocumentIds.ToDictionary(id => id, id => false) };
+                task.Progress = $"{1.0 / 3 * 100:0.##}%";
                 return TaskResult.Waiting(task, [summaryTask]);
             case "Summarize":
                 var summaryResult = dependencies.OfType<SummaryTask>().FirstOrDefault() ?? throw new Exception("Summary task result not found");
                 task.Stage = "Embed";
                 var embedTask = new EmbedTask { Documents = summaryResult.Documents.Select(x => x.Key).ToDictionary(id => id, id => false) };
+                task.Progress = $"{2.0 / 3 * 100:0.##}%";
                 return TaskResult.Waiting(task, [embedTask]);
             case "Embed":
+                task.Stage = "Finished";
+                task.Progress = "100%";
                 return TaskResult.Completed(task);
             default:
                 throw new Exception("Unknown stage");
